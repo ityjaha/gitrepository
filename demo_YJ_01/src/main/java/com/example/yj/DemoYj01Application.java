437bb08ebec792ec1eee3cb2aa3c55fdc47f5d6b
itaha <itaha@sina.com>
2018-08-05 19:02:46 +0800

    springboot项目git配置初次提交

---
+++ demo_YJ_01/src/main/java/com/example/yj/DemoYj01Application.java
@@ -0,0 +1,12 @@
+package com.example.yj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoYj01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoYj01Application.class, args);
+	}
+}
---
+++ demo_YJ_01/src/test/java/com/example/yj/DemoYj01ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.yj;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoYj01ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
